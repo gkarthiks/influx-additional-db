7ca81456c1c903a0e54fac6e3c5874889a3326e2
gkarthiks <github.gkarthiks@gmail.com>
2018-08-28 18:49:02 -0700

    travis file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM alpine:3.8
+MAINTAINER gkarthiks "github.gkarthiks@gmail.com"
 RUN apk update
 RUN apk upgrade
 RUN apk add --no-cache curl bash
---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+REPO=gkarthics/influx-additional-db
+
+DOCKER_USERNAME=${DOCKER_USERNAME}
+DOCKER_PASSWORD=${DOCKER_PASSWORD}
+
+VERSION=$TRAVIS_JOB_NUMBER
+REVISION=$TRAVIS_COMMIT
+
+docker build -f Dockerfile -t $REPO:$REVISION .
+docker tag $REPO:$REVISION $REPO:latest
+docker tag $REPO:$REVISION $REPO:1.$VERSION
+
+# docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker push $REPO
+
+docker logout
